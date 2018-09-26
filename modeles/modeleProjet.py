6b67a9ec2c5eec310a86e096c57229f8fb0db022
developpeur <developpeur@slam.sio.lla.fr>
2018-09-26 15:40:25 +0200

    MEA

--- modeles/modeleProjet.py
+++ modeles/modeleProjet.py
@@ -24,7 +24,7 @@ def getAdherant() :
 	try :
 		curseur = getConnexionBD().cursor()
 		requete = '''
-					select idAdherant, Email, mdp
+					select idAdherant, Email, mdp ,benevole
 					from Adherant
 				'''
 
@@ -47,8 +47,163 @@ def getAdherant() :
 		return None
 		
 		
-		
-def login() :
+def seConnecter( email , mdp ) :
 	try :
-		curseur = getConnexionBD.cursor()
-		r
+		curseur = getConnexionBD().cursor()
+		
+		requete = '''
+				select idAdherant, Email, mdp ,benevole
+				from Adherant
+				where Email = %s
+				and mdp = %s
+				'''
+		curseur.execute( requete , ( email , mdp ) )
+		
+		enregistrement = curseur.fetchone()
+		Personnel = {}
+		
+		if enregistrement != None :
+			Personnel[ 'idAdherant' ] = enregistrement[ 0 ]
+			Personnel[ 'Email' ] = enregistrement[ 1 ]
+			Personnel[ 'mdp' ] = enregistrement[ 2 ]
+			Personnel[ 'benevole' ] = enregistrement[ 3 ]
+		
+		curseur.close()
+		
+		return Personnel
+		
+	except :
+		return None
+		
+def livraisonLot() :
+	try:
+		curseur = getConnexionBD().cursor()
+		
+		requete = '''
+				select idLot,Lot.idProduit,Produit.libelle,Produit.uniteMesure,Lot.idProducteur,Producteur.nom,Producteur.prenom,dateLivraison
+				from Lot 
+				inner join Produit 
+				on Lot.idProduit = Produit.idProduit
+				inner join Producteur
+				on Lot.idProducteur = Producteur.idProducteur
+				'''
+		
+		curseur.execute( requete , () )
+		
+		enregistrement = curseur.fetchall()
+		Personnel = {}
+		
+		personnels = []
+	
+		for unEnregistrement in enregistrement :
+			unPersonnel = {}
+			unPersonnel[ 'idLot' ] = unEnregistrement[ 0 ]
+			unPersonnel[ 'idProduit' ] = unEnregistrement[ 1 ]
+			unPersonnel[ 'libelle' ] = unEnregistrement[ 2 ]
+			unPersonnel[ 'uniteMesure' ] = unEnregistrement[ 3 ]
+			unPersonnel[ 'idProducteur' ] = unEnregistrement[ 4 ]
+			unPersonnel[ 'nom' ] = unEnregistrement[ 5 ]
+			unPersonnel[ 'prenom' ] = unEnregistrement[ 6 ]
+			unPersonnel[ 'dateLiv' ] = unEnregistrement[ 7 ]
+
+			personnels.append( unPersonnel )
+		
+		curseur.close()
+		
+		return personnels
+		
+	except :
+		return None
+		
+		
+def getproducteur() :
+	try :
+		curseur = getConnexionBD().cursor()
+		requete = '''
+				select idProducteur,nom,prenom
+				from Producteur
+			'''
+
+		curseur.execute( requete , () )
+		enregistrements = curseur.fetchall()
+		personnels = []
+		for unEnregistrement in enregistrements :
+			unPersonnel = {}
+			unPersonnel[ 'idProducteur' ] = unEnregistrement[ 0 ]
+			unPersonnel[ 'nom' ] = unEnregistrement[ 1 ]
+			unPersonnel[ 'prenom' ] = unEnregistrement[ 2 ]
+
+			personnels.append( unPersonnel )
+		curseur.close()
+
+		return personnels
+	
+	except :
+		return None
+	
+	
+def getidProduit(libelle) :
+	try :
+		curseur = getConnexionBD().cursor()
+		requete = '''
+					select idProduit,libelle,uniteMesure
+					from Produit
+					where libelle = %s
+				'''
+		curseur.execute( requete , (libelle, ) )
+		enregistrement = curseur.fetchone()
+		produit = {}
+		if enregistrement != None :
+			produit[ 'idProduit' ] = enregistrement[ 0 ]
+			produit[ 'libelle' ] = enregistrement[ 1 ]
+			produit[ 'uniteMesure' ] = enregistrement[ 2 ]
+			
+		
+		curseur.close()
+		return produit
+		
+	except :
+		return None
+		
+		
+		
+
+def insertproduit(libelle,unMesure) :
+	try:
+		curseur = getConnexionBD().cursor()
+		
+		requete = '''
+				insert into Produit(libelle,uniteMesure) values(%s,%s);
+			
+			'''
+	
+		curseur.execute( requete , (libelle,unMesure) )
+		connexionBD.commit()
+		nbTuplesTraites = curseur.rowcount
+		curseur.close()
+
+		return nbTuplesTraites
+	
+	except :
+		
+		return None
+
+def insertLot(dateLivraison,idProduit,producteur) :
+	try:
+		curseur = getConnexionBD().cursor()
+		
+		requete = '''
+				insert into Lot(dateLivraison,idProduit,idProducteur) values(%s,%s,%s);
+			
+			'''
+		
+		curseur.execute( requete , (dateLivraison,idProduit,producteur) )
+		connexionBD.commit()
+		nbTuplesTraites = curseur.rowcount
+		curseur.close()
+
+		return nbTuplesTraites
+	
+	except :
+		
+		return None
--- projetSI4.py
+++ projetSI4.py
@@ -8,10 +8,73 @@ import requests
 app = Flask( __name__ )
 
 
+
+
+
 @app.route( '/' , methods = [ 'GET' ] )
 def index() :
 	Adherant = modeleProjet.getAdherant()
 	return render_template( 'index.html', Adherant = Adherant )
+	
+@app.route( '/adherant/session/choisir' , methods = [ 'GET' ] )
+def choisirSessionAdherant() :
+	return render_template( 'AdherantConnexion.html' , echecConnexion = False , saisieIncomplete = False )
+
+
+@app.route( '/adherant/connect/sucesser' , methods = [ 'GET' ] )
+def adherantbenevole() :
+	return render_template( 'open.html' )
+	
+
+@app.route( '/adherant/seConnecter' , methods = [ 'POST' ] )
+def seConnecterAdherant() :
+	email = request.form[ 'Email' ]
+	mdp = request.form[ 'mdp' ]
+
+	if email != '' and mdp != '' :
+		adherant = modeleProjet.seConnecter( email , mdp )
+		if len(adherant) != 0 :
+			if adherant[ 'benevole' ] == True :
+				return redirect( '/adherant/connect/sucesser' )
+				
+			else :
+				return render_template('AdherantConnexion.html', benevole = True , echecConnexion = False , saisieIncomplete = False )
+		else :
+			return render_template('AdherantConnexion.html', benevole = False , echecConnexion = True , saisieIncomplete = False )
+	else :
+		return render_template('AdherantConnexion.html', benevole = False , echecConnexion = False , saisieIncomplete = True)
+
+@app.route ( '/adherant/livraisons' , methods = [ 'GET' ] )
+def livraison() :
+	return render_template( 'livraison.html' )
+	
+@app.route ( '/adherant/livraisons/visualiser' , methods = [ 'GET' ] )
+def visualiser() :
+	lot = modeleProjet.livraisonLot()
+	return render_template( 'visualiserLot.html' , lot = lot )
+	
+@app.route ( '/adherant/livraisons/Enregistrer' , methods = [ 'GET' ] )
+def enregistrer() :
+	producteur = modeleProjet.getproducteur()
+	return render_template( 'enregistrerLot.html' , producteur = producteur)	
+	
+@app.route ( '/livraison/enregistrer/seEnregistre' , methods = [ 'POST'])
+def seEnregistrerProduit():
+	libelle = request.form[ 'libelle' ]
+	unMesure = request.form[ 'UM' ]
+	producteur = request.form[ 'producteur' ]
+	dateLivraison = request.form[ 'dateLivraison' ]
+	
+	produitInsert = modeleProjet.insertproduit(libelle,unMesure)
+	
+	if produitInsert != None :
+		idProduit = modeleProjet.getidProduit(libelle)
+		lotInsert = modeleProjet.insertLot(dateLivraison,idProduit['idProduit'],producteur)
+		lot = modeleProjet.livraisonLot()
+		return render_template( 'visualiserLot.html' , lot = lot , enregistre = True , libelle = libelle )
+	
+	else :
+		return redirect('/adherant/livraisons/Enregistrer' , probleme = True)
 
 
 if __name__ == '__main__' :
